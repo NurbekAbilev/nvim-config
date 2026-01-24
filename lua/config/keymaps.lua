53765535fb80fa1cef479e2d17d90ee3acbe94cc
Nurbek Abilev <abilev.nurbek@gmail.com>
2026-01-24 23:09:11 +0500

    <tab> as <esc>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,16 @@
 -- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 -- vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- Copy
 vim.keymap.set('v', '<C-c>', '"+y', { desc = "Copy to system clipboard" })
 vim.keymap.set('v', '<C-x>', '"+d', { desc = "Cut to system clipboard" })
 vim.keymap.set({'n', 'i'}, '<C-v>', '"+p', { desc = "Paste from system clipboard" })
 vim.keymap.set('v', '<C-v>', '"+p', { desc = "Paste from system clipboard" })
+
+-- <Esc>
+-- vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, desc = 'Escape insert mode' })
+-- vim.keymap.set('t', 'jj', '<C-\\><C-n>', { noremap = true, desc = 'Escape terminal mode' })
+-- jasldkjfasdlifj
+-- Map Tab to Escape in Insert mode
+vim.keymap.set("i", "<Tab>", "<Esc>")
+vim.keymap.set("t", "<Tab>", "<Esc>")
