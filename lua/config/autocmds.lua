94ba6448825334e7d19d46d50179f459c7cc2451
Nurbek Abilev <abilev.nurbek@gmail.com>
2026-01-24 19:15:35 +0500

    copy paste

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  pattern = "*",
+  callback = function()
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = 0 })
+    if buftype == "" then
+      vim.cmd("silent! wall")
+    end
+  end,
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+-- vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set('v', '<C-c>', '"+y', { desc = "Copy to system clipboard" })
+vim.keymap.set('v', '<C-x>', '"+d', { desc = "Cut to system clipboard" })
+vim.keymap.set({'n', 'i'}, '<C-v>', '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set('v', '<C-v>', '"+p', { desc = "Paste from system clipboard" })
